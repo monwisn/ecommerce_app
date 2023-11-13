5be20e363196615ec744846b862c76512290972d
Monika Wiśniewska <wisniewska.monika00@gmail.com>
2023-11-13 17:47:55 +0100

    add cart summary

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from store.models import Product
+
 
 def cart_summary(request) -> HttpResponse:
-    return render(request, 'cart/cart_summary.html', {})
+    products = Product.objects.all()[0:4]
+    total = Product.objects.all().values_list('price', flat=True).distinct()
+
+    return render(request, 'cart/cart_summary.html', {'products': products, 'total': total})
 
 
 def cart_add(request) -> HttpResponse:
